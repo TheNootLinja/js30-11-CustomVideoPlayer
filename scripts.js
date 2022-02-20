bfb9a333f39c946cf6289b7e0ce4f8839cb95c2d
TheNootLinja <thenootlinja@gmail.com>
2022-02-20 16:26:47 -0500

    Add code for full functionality of video player requested in challenge video

--- scripts.js
+++ scripts.js
@@ -5,25 +5,76 @@ const progressBar = playerParent.querySelector('.progress');
 const progressFill = playerParent.querySelector('.progress__filled');
 const skipButtons = playerParent.querySelectorAll('[data-skip]');
 const rangeInputs = playerParent.querySelectorAll('.player__slider');
-
-videoPlayer.onloadedmetadata = () => {
-  console.log(videoPlayer.currentTime/videoPlayer.duration)
-}
+const fullscreenButton = playerParent.querySelector('.fullscreen');
 
 const handlePlayButtonClick = () => {
-  if(videoPlayer.paused) {
-    videoPlayer.play()
+  if (videoPlayer.paused) {
+    videoPlayer.play();
   } else {
-    videoPlayer.pause()
+    videoPlayer.pause();
   }
-}
+};
 
 const updateButtonText = () => {
   const buttonIcon = videoPlayer.paused ? '►' : '❚ ❚';
   playButton.textContent = buttonIcon;
-}
+};
+
+const skipVideo = (e) => {
+  const skipValue = parseInt(e.target.getAttribute('data-skip'));
+  const videoCurrentTime = videoPlayer.currentTime;
+  const videoNewTime = videoCurrentTime + skipValue;
+  videoPlayer.currentTime = videoNewTime;
+};
+
+const adjustVideoProperties = (e) => {
+  const videoProperty = e.target.name;
+  const adjustedValue = e.target.value;
+  videoPlayer[videoProperty] = adjustedValue;
+};
+
+const handleVideoProgress = () => {
+  const progressPercent =
+    (videoPlayer.currentTime / videoPlayer.duration) * 100;
+  progressFill.style.flexBasis = `${progressPercent}%`;
+};
+
+const handleVideoScrub = (e) => {
+  const scrubDecimal = e.offsetX / progressBar.offsetWidth;
+  const scrubPercent = (e.offsetX / progressBar.offsetWidth) * 100;
+  const scrubTime = videoPlayer.duration * scrubDecimal;
+  videoPlayer.currentTime = scrubTime;
+  progressFill.style.flexBasis = `${scrubPercent}%`;
+};
+
+const handleFullscreenClick = () => {
+  if (document.fullscreenElement) {
+    document.exitFullscreen();
+    return;
+  }
+  playerParent.requestFullscreen();
+};
 
 videoPlayer.addEventListener('click', handlePlayButtonClick);
 videoPlayer.addEventListener('play', updateButtonText);
 videoPlayer.addEventListener('pause', updateButtonText);
+videoPlayer.addEventListener('timeupdate', handleVideoProgress);
+fullscreenButton.addEventListener('click', handleFullscreenClick);
 playButton.addEventListener('click', handlePlayButtonClick);
+skipButtons.forEach((button) => {
+  button.addEventListener('click', skipVideo);
+});
+rangeInputs.forEach((input) => {
+  input.addEventListener('change', adjustVideoProperties);
+});
+rangeInputs.forEach((input) => {
+  input.addEventListener('mousemove', adjustVideoProperties);
+});
+let isClicked = false;
+progressBar.addEventListener('click', handleVideoScrub);
+progressBar.addEventListener(
+  'mousemove',
+  (e) => isClicked && handleVideoScrub(e)
+);
+progressBar.addEventListener('mousedown', () => (isClicked = true));
+progressBar.addEventListener('mouseup', () => (isClicked = false));
